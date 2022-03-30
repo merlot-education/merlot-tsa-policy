aac4f032124ff5235076394765e7741ce4e9298b
Lyuben Penkovski <lyuben.penkovski@vereign.com>
2022-03-30 16:07:50 +0300

    Add mongo client dependencies to vendor

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"code.vereign.com/gaiax/tsa/golib/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Policy struct {
@@ -18,19 +19,53 @@ type Policy struct {
 	LastUpdated time.Time
 }
 
-type Storage struct{}
-
-func New() *Storage {
-	return &Storage{}
+type MongoStorage struct {
+	db         *mongo.Client
+	dbname     string
+	collection string
 }
 
-func (s *Storage) Policy(ctx context.Context, name, group, version string) (*Policy, error) {
-	key := fmt.Sprintf("%s:%s:%s", name, group, version)
+func NewMongo(db *mongo.Client, dbname, collection string) *MongoStorage {
+	return &MongoStorage{
+		db:         db,
+		dbname:     dbname,
+		collection: collection,
+	}
+}
 
-	policy, ok := policies[key]
-	if !ok {
-		return nil, errors.New(errors.NotFound, "policy not found in storage")
+func (s *MongoStorage) Policy(ctx context.Context, name, group, version string) (*Policy, error) {
+	fmt.Println("name =", name)
+	fmt.Println("group =", group)
+	fmt.Println("version =", version)
+
+	fmt.Println("dbname = ", s.dbname)
+	fmt.Println("col = ", s.collection)
+
+	result := s.db.Database(s.dbname).Collection(s.collection).FindOne(ctx, bson.M{
+		"name":    name,
+		"group":   group,
+		"version": version,
+	})
+
+	fmt.Printf("result = %#+v\n", result)
+
+	if result.Err() != nil {
+		return nil, result.Err()
 	}
 
-	return policy, nil
+	var policy Policy
+	if err := result.Decode(&policy); err != nil {
+		return nil, err
+	}
+
+	return &policy, nil
+
+	//key := fmt.Sprintf("%s:%s:%s", name, group, version)
+	//
+	//policy, ok := policies[key]
+	//if !ok {
+	//	return nil, errors.New(errors.NotFound, "policy not found in storage")
+	//}
+	//
+	//return policy, nil
 }
